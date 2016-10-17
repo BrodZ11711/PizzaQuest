3ea929e940d8a8ec74d8c6fbff0317c43463973e
Salvation1855 <Salvation1855@gmail.com>
2016-10-17 19:18:45 +1100

    6th Upload Basic working

--- player.js
+++ player.js
@@ -6,6 +6,7 @@ var RIGHT = 1;
 var ANIM_IDLE_RIGHT = 0;
 var ANIM_WALK_RIGHT = 1;
 var ANIM_JUMP_RIGHT = 2;
+var ANIM_MAX = 3;
 
 var Player = function ()
 {
@@ -14,12 +15,15 @@ var Player = function ()
 
     //this is the animations for the Main player.
     //this is the animation for idle right
-    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    this.sprite.buildAnimation(12, 6, 160, 160, 0.05,
     [0, 1, 2, 3, ]);
-    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    this.sprite.buildAnimation(12, 6, 160, 160, 0.05,
         [4, 5, 6, 7, 8, 9, 10, 11, 12, ]);
-    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    this.sprite.buildAnimation(12, 6, 160, 160, 0.05,
         [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, ]);
+
+    for (var i = 0; i < ANIM_MAX; i++)
+    { this.sprite.setAnimationOffset(i, -90, -120); }
 
     this.position = new Vector2()
     this.position.set(9 * TILE, 0 * TILE);
@@ -38,6 +42,7 @@ var Player = function ()
     this.cooldownTimer = 0;
 };
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 Player.prototype.update = function (deltaTime)
 {
@@ -61,6 +66,7 @@ Player.prototype.update = function (deltaTime)
     {
         jump = true;
     }
+
 
     var wasleft = this.velocity.x < 0;
     var wasright = this.velocity.x > 0;
