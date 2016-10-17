16e389d77086ba72995a4b91c90c8f9eee008242
Salvation1855 <Salvation1855@gmail.com>
2016-10-17 20:30:17 +1100

    7th Upload re added Chuck Norris sprite

--- player.js
+++ player.js
@@ -2,28 +2,41 @@
 var LEFT = 0;
 var RIGHT = 1;
 
-//these are the variables for the animation arrays
-var ANIM_IDLE_RIGHT = 0;
-var ANIM_WALK_RIGHT = 1;
-var ANIM_JUMP_RIGHT = 2;
-var ANIM_MAX = 3;
+var ANIM_IDLE_LEFT = 0;
+var ANIM_JUMP_LEFT = 1;
+var ANIM_WALK_LEFT = 2;
+var ANIM_IDLE_RIGHT = 3;
+var ANIM_JUMP_RIGHT = 4;
+var ANIM_WALK_RIGHT = 5;
+var ANIM_MAX = 6;
 
 var Player = function ()
 {
     //this is for the animation of the sprite.
-    this.sprite = new Sprite("player.png");
-
-    //this is the animations for the Main player.
+    this.sprite = new Sprite("ChuckNorris.png");
+    //this is the animation for idle left
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [0, 1, 2, 3, 4, 5, 6, 7]);
+    //this is the animation for jump left
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [8, 9, 10, 11, 12]);
+    //this is the animation for walking left
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26]);
     //this is the animation for idle right
-    this.sprite.buildAnimation(12, 6, 160, 160, 0.05,
-    [0, 1, 2, 3, ]);
-    this.sprite.buildAnimation(12, 6, 160, 160, 0.05,
-        [4, 5, 6, 7, 8, 9, 10, 11, 12, ]);
-    this.sprite.buildAnimation(12, 6, 160, 160, 0.05,
-        [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, ]);
-
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [52, 53, 54, 55, 56, 57, 58, 59]);
+    //this is the animation for the jump right
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [60, 61, 62, 63, 64]);
+    //this is the animation for walking right
+    this.sprite.buildAnimation(12, 8, 165, 126, 0.05,
+    [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78]);
+    //this makes it so it allows us to set the same offset for all animations
     for (var i = 0; i < ANIM_MAX; i++)
-    { this.sprite.setAnimationOffset(i, -90, -120); }
+    {
+        this.sprite.setAnimationOffset(i, -55, -87);
+    }
 
     this.position = new Vector2()
     this.position.set(9 * TILE, 0 * TILE);
@@ -42,7 +55,6 @@ var Player = function ()
     this.cooldownTimer = 0;
 };
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 
 Player.prototype.update = function (deltaTime)
 {
@@ -66,7 +78,6 @@ Player.prototype.update = function (deltaTime)
     {
         jump = true;
     }
-
 
     var wasleft = this.velocity.x < 0;
     var wasright = this.velocity.x > 0;
@@ -92,9 +103,9 @@ Player.prototype.update = function (deltaTime)
     {
         ddy = ddy - JUMP; // apply an instantaneous (large) vertical impulse
         this.jumping = true;
-
+        
         //this is to get the jumping animation correct
-        if (this.direction == LEFT)
+        if(this.direction == LEFT)
             this.sprite.setAnimation(ANIM_JUMP_LEFT)
         else
             this.sprite.setAnimation(ANIM_JUMP_RIGHT)
@@ -134,10 +145,8 @@ Player.prototype.update = function (deltaTime)
     // If the player has vertical velocity, then check to see if they have hit a platform
     // below or above, in which case, stop their vertical velocity, and clamp their
     // y position:
-    if (this.velocity.y > 0)
-    {
-        if ((celldown && !cell) || (celldiag && !cellright && nx))
-        {
+    if (this.velocity.y > 0) {
+        if ((celldown && !cell) || (celldiag && !cellright && nx)) {
             // clamp the y position to avoid falling into platform below
             this.position.y = tileToPixel(ty);
             this.velocity.y = 0; // stop downward velocity
@@ -146,10 +155,8 @@ Player.prototype.update = function (deltaTime)
             ny = 0; // no longer overlaps the cells below
         }
     }
-    else if (this.velocity.y < 0)
-    {
-        if ((cell && !celldown) || (cellright && !celldiag && nx))
-        {
+    else if (this.velocity.y < 0) {
+        if ((cell && !celldown) || (cellright && !celldiag && nx)) {
             // clamp the y position to avoid jumping into platform above
             this.position.y = tileToPixel(ty + 1);
             this.velocity.y = 0; // stop upward velocity
@@ -159,25 +166,21 @@ Player.prototype.update = function (deltaTime)
             ny = 0; // player no longer overlaps the cells below
         }
     }
-    if (this.velocity.x > 0)
-    {
-        if ((cellright && !cell) || (celldiag && !celldown && ny))
-        {
+    if (this.velocity.x > 0) {
+        if ((cellright && !cell) || (celldiag && !celldown && ny)) {
             // clamp the x position to avoid moving into the platform we just hit
             this.position.x = tileToPixel(tx);
             this.velocity.x = 0; // stop horizontal velocity
         }
     }
-    else if (this.velocity.x < 0)
-    {
-        if ((cell && !cellright) || (celldown && !celldiag && ny))
-        {
+    else if (this.velocity.x < 0) {
+        if ((cell && !cellright) || (celldown && !celldiag && ny)) {
             // clamp the x position to avoid moving into the platform we just hit
             this.position.x = tileToPixel(tx + 1);
             this.velocity.x = 0; // stop horizontal velocity
         }
     }
-
+    
     //this will make it so if you press left on keyboard the animation will go left.
     if (keyboard.isKeyDown(keyboard.KEY_LEFT) == true)
     {
@@ -188,24 +191,26 @@ Player.prototype.update = function (deltaTime)
             this.sprite.setAnimation(ANIM_WALK_LEFT);
     }
 
-        //this will make it so if you press left on keyboard the animation will go right.
-    else if (keyboard.isKeyDown(keyboard.KEY_RIGHT) == true)
-    {
+    //this will make it so if you press left on keyboard the animation will go right.
+    else if (keyboard.isKeyDown(keyboard.KEY_RIGHT) == true) {
         right = true;
         this.direction = RIGHT;
         if (this.sprite.currentAnimation != ANIM_WALK_RIGHT &&
             this.jumping == false)
             this.sprite.setAnimation(ANIM_WALK_RIGHT);
     }
-        //this will make it so it will return the animation in to its idle form
-    else {
+    //this will make it so it will return the animation in to its idle form
+    else
+    {
         if (this.jumping == false && this.falling == false)
         {
-            if (this.direction == LEFT) {
+            if (this.direction == LEFT)
+            {
                 if (this.sprite.currentAnimation != ANIM_IDLE_LEFT)
                     this.sprite.setAnimation(ANIM_IDLE_LEFT);
             }
-            else {
+            else
+            {
                 if (this.sprite.currentAnimation != ANIM_IDLE_RIGHT)
                     this.sprite.setAnimation(ANIM_IDLE_RIGHT);
             }
@@ -214,19 +219,20 @@ Player.prototype.update = function (deltaTime)
     if (keyboard.isKeyDown(keyboard.KEY_UP) == true)
     {
         jump = true;
-        if (left == true) {
+        if (left == true)
+        {
             this.sprite.setAnimation(ANIM_JUMP_LEFT);
         }
-        if (right == true) {
+        if (right == true)
+        {
             this.sprite.setAnimation(ANIM_JUMP_RIGHT);
         }
     }
-    
+    //this will make it so everytime the player shoots it will make the shooting sound.
     if (keyboard.isKeyDown(keyboard.KEY_UP) == true)
     {
         jump = true;
     }
-    //this will make it so everytime the player shoots it will make the shooting sound.
     if (this.cooldownTimer > 0)
     {
         this.cooldownTimer -= deltaTime;
@@ -240,6 +246,9 @@ Player.prototype.update = function (deltaTime)
 
 }
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 Player.prototype.draw = function ()
 {
     //this will draw the animation
@@ -250,3 +259,4 @@ Player.prototype.draw = function ()
     context.rotate(this.rotation);
     context.restore();
 }
+
